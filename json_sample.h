7c72ae6f04d878959cbd99178fee579bac9a8fbf
AndrewSh <sheeftz@gmail.com>
2021-12-31 07:36:22 +0300

    - comment fix

--- json_sample.h
+++ json_sample.h
@@ -7,7 +7,7 @@
 #define JSON_STRUCT(Name) REFLECTION_STRUCT(JsonMetaData, Method, Name)
 
 using namespace std; // using namespace in a header file is not the best idea
-using namespace Reflection; // but it makes it easier, since c++ doesn't support nested namespace usings
+using namespace Reflection; // but it makes it easier because c++ doesn't support nested namespace usings
 
 template<typename>
 struct JsonReadWrite; // it's convenient to keep Read and Write implementations in a single templated struct
